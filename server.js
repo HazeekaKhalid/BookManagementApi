448daf3fb55880b9277e6b3f3f9ff7771568ec82
Hazeeka Khalid <hazeekakhalid8@gmail.com>
2026-09-15 16:39:39 +0300

    Change root message in feature branch

--- server.js
+++ server.js
@@ -59,12 +59,16 @@ app.delete("/books/:id", (req, res) => {
 res.json({ message: "Book deleted", book: deletedBook[0] }); 
 }); 
 
+app.get('/', (req, res) => res.send("Hello from conflict branch"));
+
 app.get("/health", (req, res) => {
     res.json({
         status: "OK",
         message: "API is running"
     });
 });
+
+app.get('/', (req, res) => res.send("Original message"));
 
 app.listen(PORT, () => { 
 console.log(`Server running on http://localhost:${PORT}`); 
