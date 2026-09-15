1de20b28172ab2c1ba2924be9c86c1017a0e1a11
Hazeeka Khalid <hazeekakhalid8@gmail.com>
2026-09-15 16:55:37 +0300

    Configure environment variables

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const express = require("express"); 
+require("dotenv").config(); 
 const app = express(); 
- 
 app.use(express.json()); 
+const PORT = process.env.PORT || 3000; 
  
 let books = [ 
   { id: 1, title: "The Harry Potter & The Philosopher's Stone", author: "J.K. Rowling" }, 
   { id: 2, title: "Diary of a Wimpy Kid", author: "Jeff Kinney" } 
 ]; 
- 
-const PORT = 3000; 
  
 app.get("/", (req, res) => { 
   res.send("Welcome to Book API"); 
